9c70445b454bf0c4a33b7abae9aed26fa6694cdb
Ines Montani <ines@ines.io>
2019-04-21 13:58:37 +0200

    Fix typo (closes #7)

--- exercises/test_02_09.py
+++ exercises/test_02_09.py
@@ -1,7 +1,7 @@
 def test():
     assert (
-        "spacy.load('en_core_web_sm')" in __solution__
-        or 'spacy.load("en_core_web_sm")' in __solution__
+        "spacy.load('en_core_web_md')" in __solution__
+        or 'spacy.load("en_core_web_md")' in __solution__
     ), "Are you loading the medium model correctly?"
     assert "doc[1].vector" in __solution__, "Are you getting the correct vector?"
     __msg__.good(
