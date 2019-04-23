3e54a4c2ee00f5fcab9a9d61ad58090e53f2683b
Ines Montani <ines@ines.io>
2019-04-24 01:52:46 +0200

    Make 03_09_01 test less strict
    Check for values and not for string match, since doc[-2] (or whatever really) would also be valid

--- exercises/test_03_09_01.py
+++ exercises/test_03_09_01.py
@@ -4,9 +4,10 @@ def test():
     ), "Did you register the extension on the token?"
     ext = Token.get_extension("is_country")
     assert ext[0] == False, "Did you set the default value correctly?"
-    assert (
-        "doc[3]._.is_country = True" in __solution__
-    ), "Did you change the value for the right token?"
+    country_values = [False, False, False, True, False]
+    assert [
+        t._.is_country for t in doc
+    ] == country_values, "Did you change the value for the right token?"
     assert (
         "print([(token.text, token._.is_country)" in __solution__
     ), "Are you printing the right token attributes?"
