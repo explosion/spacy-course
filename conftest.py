9fcb7404e9db94c0d291ea8da6eb60be925a33d2
Ines Montani <ines@ines.io>
2020-04-15 12:40:57 +0200

    Add general model / base assumption tests

--- conftest.py
+++ conftest.py
@@ -3,7 +3,7 @@ import shutil
 from pathlib import Path
 from wasabi import msg
 import srsly
-import sys
+import shutil
 from spacy.tokens.underscore import Underscore
 
 
@@ -12,6 +12,7 @@ EXERCISES_DIR = "exercises"
 META_FILE = "meta.json"
 PYTEST_TEMPLATE = "pytestTemplate"
 LANG_CLI_ARG = "lang"
+GENERAL_TEST = "test_general.py"
 
 
 @pytest.fixture(scope="module", autouse=True)
@@ -45,7 +46,10 @@ def get_source_files(lang):
                     solution_name = f"solution_{py_file.name.split('test_')[1]}"
                     solution_file = lang_path / solution_name
                     if not solution_file.exists():
-                        msg.warn(f"Didn't find solution for test: {py_file.stem} ({lang_path})")
+                        if py_file.name == GENERAL_TEST:
+                            yield (lang_name, py_file, None)
+                        else:
+                            msg.warn(f"Didn't find solution for test: {py_file.stem} ({lang_path})")
                     else:
                         yield (lang_name, py_file, solution_file)
 
@@ -67,6 +71,15 @@ def pytest_sessionstart(session):
     meta = srsly.read_json(META_FILE)
     n_files = 0
     for test_lang, test_file, solution_file in get_source_files(lang):
+        test_root = test_dir / test_lang
+        if not test_root.exists():
+            test_root.mkdir()
+            init_path = test_root / "__init__.py"
+            init_path.touch()
+        if not solution_file:  # general test file, just copy it over
+            shutil.copy(str(test_file), str(test_root / test_file.name))
+            n_files += 1
+            continue
         with test_file.open("r", encoding="utf8") as f:
             test_code = f.read()
         with solution_file.open("r", encoding="utf8") as f:
@@ -74,11 +87,6 @@ def pytest_sessionstart(session):
         full_code = format_test(
             test_file.stem, meta[PYTEST_TEMPLATE], test_code, solution_code
         )
-        test_root = test_dir / test_lang
-        if not test_root.exists():
-            test_root.mkdir()
-            init_path = test_root / "__init__.py"
-            init_path.touch()
         test_path = test_root / test_file.name
         with test_path.open("w", encoding="utf8") as f:
             f.write(full_code)
