8b244e52769c9c9d085410f58114aab05c90e506
Maria Camila Gutierrez <mariacamilagl30@gmail.com>
2020-05-19 21:00:33 +0200

    ✨ Chapter 1 exercises + solutions + tests

--- exercises/es/test_01_11.py
+++ exercises/es/test_01_11.py
@@ -17,19 +17,19 @@ def test():
         len(pattern[0]) == 1 and len(pattern[1]) == 1
     ), "Cada entrada en el patrón debería tener solo un key."
     assert any(
-        pattern[0].get(key) == "iPhone" for key in ["text", "TEXT"]
+        pattern[0].get(key) == "adidas" for key in ["text", "TEXT"]
     ), "¿Estás encontrando el texto del token?"
     assert any(
-        pattern[1].get(key) == "X" for key in ["text", "TEXT"]
+        pattern[1].get(key) == "ZX" for key in ["text", "TEXT"]
     ), "¿Estás encontrando el texto del token?"
     assert (
-        'matcher.add("IPHONE_X_PATTERN"' in __solution__
+        'matcher.add("ADIDAS_ZX_PATTERN"' in __solution__
     ), "¿Estás añadiendo el patrón correctamente?"
     assert (
         "matches = matcher(doc)" in __solution__
     ), "¿Estás llamando al matcher sobre el doc?"
 
     __msg__.good(
-        "¡Bien hecho! Encontraste un resultado exitosamente: los tokens en doc[1:3] "
-        'que describen el span de "iPhone X".'
+        "¡Bien hecho! Encontraste un resultado exitosamente: los tokens en doc[14:16] "
+        'que describen el span de "adidas ZX".'
     )
