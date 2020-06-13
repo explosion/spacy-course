49a76627370317e8d704d0d315ee2fb98107cfc9
Ines Montani <ines@ines.io>
2020-06-13 13:59:30 +0200

    Auto-format [ci skip]

--- exercises/de/test_01_09.py
+++ exercises/de/test_01_09.py
@@ -1,6 +1,8 @@
 def test():
     assert "for ent in doc.ents" in __solution__, "Iterierst du über die Entitäten?"
-    assert iphone_se.text == "iPhone SE", "Bist du dir sicher, dass iphone_se die richtigen Tokens erfasst?"
+    assert (
+        iphone_se.text == "iPhone SE"
+    ), "Bist du dir sicher, dass iphone_se die richtigen Tokens erfasst?"
 
     __msg__.good(
         "Super! Natürlich musst du das hier nicht immer von Hand machen. In der "
