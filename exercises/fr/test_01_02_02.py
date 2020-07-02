fe936c1ea8304cb15886534498aef65ba6ca9196
datakime <datakime@gmail.com>
2020-07-02 17:28:46 +0200

    Update test_01_02_02.py
    Becomes test of English example

--- exercises/fr/test_01_02_02.py
+++ exercises/fr/test_01_02_02.py
@@ -1,13 +1,13 @@
 def test():
     import spacy.tokens
-    import spacy.lang.de
+    import spacy.lang.en
 
     assert isinstance(
-        nlp, spacy.lang.de.German
-    ), "L'objet nlp doit être une instance de la classe German."
+        nlp, spacy.lang.en.English
+    ), "L'objet nlp doit être une instance de la classe English."
     assert isinstance(
         doc, spacy.tokens.Doc
     ), "As-tu traité le texte avec l'objet nlp pour créer un doc ?"
     assert "print(doc.text)" in __solution__, "As-tu affiché doc.text ?"
 
-    __msg__.good("Sehr gut! :)")
+    __msg__.good("Bien joué !")
