9188510470bb0f2251f24abfae449de2ee3ffac2
datakime <datakime@gmail.com>
2020-07-02 17:29:58 +0200

    Update exc_01_02_01.py
    Becomes French example

--- exercises/fr/exc_01_02_01.py
+++ exercises/fr/exc_01_02_01.py
@@ -1,11 +1,11 @@
-# Importe la classe de langue "English"
+# Importe la classe de langue "French"
 from spacy.lang.____ import ____
 
 # Crée l'objet nlp
 nlp = ____
 
 # Traite un texte
-doc = nlp("This is a sentence.")
+doc = nlp("Ceci est une phrase.")
 
 # Affiche le texte du document
 print(____.text)
