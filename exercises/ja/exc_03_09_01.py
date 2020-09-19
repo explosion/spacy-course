7e5ec5ddb5c80dc561c3d0a263022b3f9a6a4299
mei <mei28aquarius@gmail.com>
2020-09-19 16:23:04 +0900

    chapter3 問題ファイル

--- exercises/ja/exc_03_09_01.py
+++ exercises/ja/exc_03_09_01.py
@@ -1,14 +1,14 @@
-from spacy.lang.en import English
+from spacy.lang.ja import Japanese
 from spacy.tokens import Token
 
-nlp = English()
+nlp = Japanese()
 
 # デフォルト値がFalseである拡張属性「is_country」をトークンに追加
 ____.____(____, ____=____)
 
 # テキストを処理し、「Spain」のトークンについてis_country属性をTrueにする
-doc = nlp("I live in Spain.")
+doc = nlp("私はスペインに住んでいます。")
 ____ = True
 
-# すべてのトークンについて、文字列とis_country属性をプリント
+# すべてのトークンについて、文字列とis_country属性を表示
 print([(____, ____) for token in doc])
