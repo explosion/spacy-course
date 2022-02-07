8ee90c942b9179d00f79096b0ccb96a8bbd5c452
Ines Montani <ines@ines.io>
2022-02-07 16:22:53 +0100

    Fix typos

--- exercises/pt/test_04_04.py
+++ exercises/pt/test_04_04.py
@@ -1,9 +1,9 @@
 def test():
-    assert ('spacy.blank("en")' in __solution__  ), "Você inicializou um fluxo de processamento em Inglês vazio?"
-    assert ( "DocBin(docs=docs)" in __solution__  ), VOcê criou o DocBin corretamente?"
-    assert ("doc_bin.to_disk(" in __solution__ ), "Você utilizou o método to_disk?"
-    assert ("train.spacy" in __solution__ ), "Você criou um arquivo com o nome correto?"
+    assert (
+        'spacy.blank("en")' in __solution__
+    ), "Você inicializou um fluxo de processamento em Inglês vazio?"
+    assert "DocBin(docs=docs)" in __solution__, "Você criou o DocBin corretamente?"
+    assert "doc_bin.to_disk(" in __solution__, "Você utilizou o método to_disk?"
+    assert "train.spacy" in __solution__, "Você criou um arquivo com o nome correto?"
 
-    __msg__.good(
-        "Muito bem! Tudo certo por aqui."
-    )
+    __msg__.good("Muito bem! Tudo certo por aqui.")
