3a156e14c07edd2606144fa9c5bc091ba3d0c38b
Ines Montani <ines@ines.io>
2025-02-07 08:26:49 +0100

    Update French

--- exercises/fr/solution_01_11.py
+++ exercises/fr/solution_01_11.py
@@ -4,13 +4,13 @@ import spacy
 from spacy.matcher import Matcher
 
 nlp = spacy.load("fr_core_news_sm")
-doc = nlp("Apple : le nouveau modèle X Pro attendu pour l'été.")
+doc = nlp("Apple : le nouveau modèle x pro attendu pour l'été.")
 
 # Initialise le matcher avec le vocabulaire partagé
 matcher = Matcher(nlp.vocab)
 
-# Crée un motif qui recherche les deux tokens : "X" et "Pro"
-pattern = [{"TEXT": "X"}, {"TEXT": "Pro"}]
+# Crée un motif qui recherche les deux tokens : "x" et "pro"
+pattern = [{"TEXT": "x"}, {"TEXT": "pro"}]
 
 # Ajoute le motif au matcher
 matcher.add("IPHONE_X_PATTERN", [pattern])
--- exercises/fr/test_01_09.py
+++ exercises/fr/test_01_09.py
@@ -1,6 +1,6 @@
 def test():
     assert "for ent in doc.ents" in __solution__, "Itères-tu sur les entités ?"
-    assert x_pro.text == "X Pro", "Es-tu certain que x_pro contient les bons tokens ?"
+    assert x_pro.text == "x pro", "Es-tu certain que x_pro contient les bons tokens ?"
 
     __msg__.good(
         "Parfait ! Bien sur, tu n'as pas besoin de faire cela manuellement à chaque fois."
